ea21053bf161fccb7f9d251fecc60e0036afecd9
prasadkamtekar <saiprasad.kamtekar777@gmail.com>
2026-02-02 23:05:50 +0530

    data stored done

--- Notes-App/src/App.jsx
+++ Notes-App/src/App.jsx
@@ -1,38 +1,61 @@
 
-import Recent from './recentNotes.jsx'
+import { useState } from 'react'
+
 
 function App() {
+
+  const [title, setTitle] = useState('');
+
+  const [detail, setDetail] = useState('');
+  const [arr, setArr] = useState([]);
+
+  const insert = () => {
+    setArr(p => [...p, [title, detail]]);
+    console.log(title, detail)
+  }
+
+
   return (
     <>
       <div className='main-container'>
-        
+
         <div className='input-container'>
           <h1>Add Notes</h1>
 
           <input
+            onChange={(e) => { setTitle(e.target.value); }}
             type="text"
             placeholder='Enter Note'
+            value={title}
             className='note'
           />
 
           <textarea
+            onChange={(e) => { setDetail(e.target.value); }}
             placeholder='Enter Details'
+            value={detail}
             className="detail"
           />
 
-          <button className='btn'>Add Note</button>
+          <button className='btn' onClick={insert}>Add Note</button>
         </div>
 
         <div className='recent-c'>
           <h1>Recent Notes</h1>
-          <div className='recent'>
-          <Recent />
-          <Recent />
-          <Recent />
-          <Recent />
-          <Recent />
-          <Recent />
+          <div className="recent">
+            {arr.map((row, i) => (
+              <div key={i} >
+                {row.map((item, j) => (
+                  <div key={j} className="recent-container">
+                    <h1>{item.title}</h1>
+                    <p>{item.detail}</p>
+                    <button>Delete</button>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
+
         </div>
 
       </div>
