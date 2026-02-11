e57d6dfd1254f2b3d98284678536b31525f4a7e5
prasadkamtekar <saiprasad.kamtekar777@gmail.com>
2026-02-11 22:40:42 +0530

    Noted app completed

--- Notes-App/src/App.jsx
+++ Notes-App/src/App.jsx
@@ -5,29 +5,31 @@ import { useEffect, useState } from 'react'
 function App() {
 
   const [title, setTitle] = useState("");
-  
+
 
   const [detail, setDetail] = useState('');
-  const [arr, setArr] = useState(()=> {
+  const [arr, setArr] = useState(() => {
     const data = localStorage.getItem("notes");
     return data ? JSON.parse(data) : [];
   });
-  
-  useEffect(()=> {
-    localStorage.setItem("notes" , JSON.stringify(arr));
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(arr));
   }, [arr]);
 
   const insert = () => {
     const copyArr = [...arr];
     copyArr.push({ titles: `${title}`, details: `${detail}` });
     setArr(copyArr);
-    
+    setTitle("");
+    setDetail("");
+
   }
 
-  const deleteNote = (idx)=> {
-         const copyArr = [...arr];
-         copyArr.splice(idx,1);
-         setArr(copyArr);   
+  const deleteNote = (idx) => {
+    const copyArr = [...arr];
+    copyArr.splice(idx, 1);
+    setArr(copyArr);
   }
 
   return (
@@ -57,18 +59,18 @@ function App() {
 
         <div className='recent-c'>
           <h1>Recent Notes</h1>
-          
-            <div className="recent">
-              {arr.map((notes, idx) => {
-                return (<div className="recent-container" key={idx}>
-                    <>
-                      <h1>{notes.titles}</h1>
-                      <p>{notes.details}</p>
-                      <button   onClick={()=> deleteNote(idx)}>Delete</button>
-                    </>
-                </div>)
-              })}
-            </div>
+
+          <div className="recent">
+            {arr.map((notes, idx) => {
+              return (<div className="recent-container" key={idx}>
+                <>
+                  <h1>{notes.titles}</h1>
+                  <p>{notes.details}</p>
+                  <button onClick={() => deleteNote(idx)}>Delete</button>
+                </>
+              </div>)
+            })}
+          </div>
         </div>
 
       </div>
